cf8c92341bcb45b71438388bd9938fc929bb9a82
James <40529002+EchoofthePast@users.noreply.github.com>
2020-10-26 15:21:29 +0200

    Add files via upload

---
+++ godata.go
@@ -0,0 +1,38 @@
+package goget
+
+
+//Access the applications exported data structure
+type Access struct {
+	Status string
+}
+
+
+
+
+//DataStruct the applications exported data structure
+type DataStruct struct {
+	Image       string
+	Name        string
+	Surname     string
+	Both        string
+	Middlename  string
+	ID          string
+	Number      string
+	Age         string
+	Tel			string
+	Cell		string
+	Information string
+	Medication  string
+	URL         string
+
+	Address string
+	Suburb string
+	City string
+	Province string
+	Country string
+	Add3 string
+	Add4 string
+	Doc0 string
+	Doc1 string
+	Doc2 string
+}
